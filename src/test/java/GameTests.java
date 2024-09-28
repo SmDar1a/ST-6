12164572bd9cb6e11a2691693f3e0d04ead43ac8
SmDar1a <Smirnova.darya.vl@gmail.com>
2024-09-28 17:48:18 +0300

    update lab

--- src/test/java/GameTests.java
+++ src/test/java/GameTests.java
@@ -3,86 +3,89 @@ package org.example;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
 
-import java.awt.*;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GameTests {
 
-    private static Game game;
+    static Game game;
 
     @BeforeAll
-    public static void setUp() {
+    static void setup() {
         game = new Game();
     }
 
     @Test
-    public void testDrawStateDetection() {
-        game.board = new char[]{'X', 'O', 'X', 'O', 'X', 'O', 'O', 'X', 'O'};
-        assertEquals(State.DRAW, game.checkState(game.board));
+    @DisplayName("Initial game state is PLAYING")
+    public void testInitialGameState() {
+        assertEquals(State.PLAYING, game.state);
     }
 
     @Test
-    public void testWinningCombinationDetectionForO() {
-        game.board = new char[]{'O', 'O', 'O', ' ', ' ', ' ', ' ', ' ', ' '};
-        game.symbol = 'O';
-        assertEquals(State.OWIN, game.checkState(game.board));
-    }
-
-    @Test
-    public void testWinningDiagonalCombinationDetectionForX() {
-        game.board = new char[]{' ', ' ', 'X', ' ', 'X', ' ', 'X', ' ', ' '};
+    @DisplayName("Check win for X on the first row")
+    public void testXWinFirstRow() {
+        game.board[0] = 'X';
+        game.board[1] = 'X';
+        game.board[2] = 'X';
         game.symbol = 'X';
         assertEquals(State.XWIN, game.checkState(game.board));
     }
 
     @Test
-    public void testWinningDiagonalCombinationDetectionForO() {
-        game.board = new char[]{' ', ' ', 'O', ' ', 'O', ' ', 'O', ' ', ' '};
+    @DisplayName("Check win for O on the diagonal")
+    public void testOWinDiagonal() {
+        game.board[0] = 'O';
+        game.board[4] = 'O';
+        game.board[8] = 'O';
         game.symbol = 'O';
         assertEquals(State.OWIN, game.checkState(game.board));
-    }    
+    }
 
     @Test
-    public void testWinningVerticalCombinationDetectionForX() {
-        game.board = new char[]{'X', ' ', ' ', 'X', ' ', ' ', 'X', ' ', ' '};
+    @DisplayName("Check draw state")
+    public void testDrawState() {
+        char[] drawBoard = {'X', 'O', 'X', 'X', 'O', 'O', 'O', 'X', 'X'};
+        System.arraycopy(drawBoard, 0, game.board, 0, 9);
         game.symbol = 'X';
-        assertEquals(State.XWIN, game.checkState(game.board));
-    }
-
-    @Test
-    public void testWinningVerticalCombinationDetectionForO() {
-        game.board = new char[]{'O', ' ', ' ', 'O', ' ', ' ', 'O', ' ', ' '};
-        game.symbol = 'O';
-        assertEquals(State.OWIN, game.checkState(game.board));
-    }
-
-    @Test
-    public void testStateWithTwoFilledRows() {
-        game.board = new char[]{'X', 'X', 'X', 'O', 'O', 'O', ' ', ' ', ' '};
-        assertEquals(State.XWIN, game.checkState(game.board));
-    }
-
-   @Test
-    public void testStateWithFilledBoardButNoWinningCombination() {
-        game.board = new char[]{'X', 'O', 'X', 'O', 'X', 'O', 'O', 'X', 'O'};
         assertEquals(State.DRAW, game.checkState(game.board));
-    } 
-
-    @Test
-    public void testStateWithOneFilledRow() {
-        game.board = new char[]{'X', 'X', 'X', ' ', ' ', ' ', ' ', ' ', ' '};
-        assertEquals(State.XWIN, game.checkState(game.board));
     }
 
     @Test
-    public void testStateWithOneFilledCell() {
-        game.board = new char[]{'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
-        assertEquals(State.PLAYING, game.checkState(game.board));
+    @DisplayName("Evaluate position: X winning")
+    public void testEvaluatePositionXWin() {
+        game.board[0] = 'X';
+        game.board[1] = 'X';
+        game.board[2] = 'X';
+        game.symbol = 'X';
+        assertEquals(Game.INF, game.evaluatePosition(game.board, game.player1));
     }
 
+    @Test
+    @DisplayName("MiniMax: Best move for player O")
+    public void testMiniMaxMove() {
+        game.board[0] = 'X';
+        game.board[1] = 'O';
+        game.board[2] = 'X';
+        game.board[3] = 'X';
+        game.board[4] = 'O';
+        game.board[5] = ' ';
+        game.board[6] = ' ';
+        game.board[7] = ' ';
+        game.board[8] = ' ';
+        int move = game.MiniMax(game.board, game.player2);
+        assertTrue(move > 0);
+    }
+
+    @Test
+    @DisplayName("MaxMove returns a valid position")
+    public void testMaxMove() {
+        game.board[0] = 'X';
+        game.board[1] = 'O';
+        game.board[2] = 'X';
+        game.symbol = 'O';
+        int moveValue = game.MaxMove(game.board, game.player1);
+        assertTrue(moveValue <= Game.INF);
+    }
 }
